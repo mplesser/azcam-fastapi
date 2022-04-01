7dc2b9454c308137571de3d95b04d1223041c637
Michael Lesser <mlesser@arizona.edu>
2022-04-01 14:51:23 -0700

    cleanup

---
+++ azcam_fastapi/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "22.0.0"
+__version_info__ = tuple(int(i) for i in __version__.split(".") if i.isdigit())
--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name="azcam-fastapi",
-    version="21.0.4",
-    description="azcam extension for fastapi web server",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Michael Lesser",
-    author_email="mlesser@arizona.edu",
-    keywords="python",
-    packages=find_packages(),
-    zip_safe=False,
-    url="https://mplesser.github.io/azcam/",
-    install_requires=["azcam"],
-    include_package_data=True,
-)
+if __name__ == "__main__":
+    setup()
